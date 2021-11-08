8e8c46309473957f85be91e15f2603c215cb6956
ironW-ill <rlaqudtn5098@naver.com>
2021-11-08 21:18:22 +0900

    [FRONT] axios 탑재

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
-        text 
+        text
       >
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
@@ -46,5 +46,9 @@ export default {
   data: () => ({
     //
   }),
+
+  created() {
+    console.log(this.$axios);
+  },
 };
 </script>
