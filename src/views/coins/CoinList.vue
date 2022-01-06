106703a32af4d71df2c278ef40d5c3116b7835e0
김병수 <bkim8@uangel.com>
2022-01-06 16:24:15 +0900

    코인 목록 v1.0

--- src/views/coins/CoinList.vue
+++ src/views/coins/CoinList.vue
@@ -4,9 +4,13 @@
       ><template>
         <v-card>
           <v-card-title>
-            <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
+            <v-text-field append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
           </v-card-title>
-          <v-data-table :headers="headers" :items="coinList" :search="search"></v-data-table>
+          <v-data-table :headers="headers" :items="coinList">
+            <template v-slot:[`item.image`]="{ item }">
+              <v-img max-width="35" :src="item.image" />
+            </template>
+          </v-data-table>
         </v-card>
       </template>
     </v-col>
@@ -18,19 +22,23 @@ export default {
   name: 'CoinList',
   data() {
     return {
-      search: '',
       coinList: [],
       headers: [
+        { text: '#', value: 'image', align: 'left' },
         { text: '코인명', value: 'id', align: 'left' },
-        { text: '심볼명', value: 'symbol', aligh: 'left' }
+        { text: '심볼명', value: 'symbol', aligh: 'left' },
+        { text: '현재가', value: 'current_price', aligh: 'left' }
       ]
     }
   },
   created() {
-    this.$axios.get(this.$coingecko + '/v3/coins/list').then((res) => {
-      const resData = res.data
-      this.coinList = resData
-    })
+    this.$axios
+      .get(this.$coingecko + '/v3/coins/markets', { params: { vs_currency: 'krw', per_page: '100', page: '1', sparkline: false } })
+      .then((res) => {
+        const resData = res.data
+        console.log(resData)
+        this.coinList = resData
+      })
   }
 }
 </script>
